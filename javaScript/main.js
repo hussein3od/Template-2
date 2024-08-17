75fb2ac82ac604cfa403643c6db02799ca780241
hussein3od <hhssing0@gmail.com>
2024-08-17 12:36:42 +0100

    handle Active and all section

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,6 +1,6 @@
 const parent = document.querySelector(".parent")
 let list = document.querySelector(".list")
-
+list.classList.add("class")
 const getGategores = async () => {
     let data = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
     data = await data.json();
@@ -12,21 +12,66 @@ const getGategores = async () => {
             getRespes(li.innerHTML)
         })
     }
+    let text = document.querySelectorAll(".class li");
+    activeClass(text)
 }
 
 const getRespes = (str) => {
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${str}`)
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${str}`)
             .then((result) => {
                 return result.json();
             }).then((d) => {
                 console.log(d)
                 parent.innerHTML = ""
                 for(let m = 0; m < d.meals.length; m++) {
-                    let meals = document.createElement("div")
-                    meals.innerHTML = d.meals[m].strMeal
-                    parent.appendChild(meals)
+                    let mealName = document.createElement("h2")
+                    let img = document.createElement("img");
+                    let box = document.createElement("div")
+                    box.classList.add("box")
+                    mealName.classList.add("meal-name")
+                    mealName.innerHTML = d.meals[m].strMeal
+                    img.src = d.meals[m].strMealThumb
+                    box.appendChild(mealName)
+                    box.appendChild(img)
+                    parent.appendChild(box)
                 }
             })
+        }
+        
+getGategores()
+
+const all = async () => {
+    let data = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s")
+    data = await data.json();
+    for(let m = 0; m < data.meals.length; m++){
+        let mealName = document.createElement("h2")
+        let img = document.createElement("img");
+        let box = document.createElement("div")
+        box.classList.add("box")
+        mealName.classList.add("meal-name")
+        mealName.innerHTML = data.meals[m].strMeal
+        img.src = data.meals[m].strMealThumb
+        box.appendChild(mealName)
+        box.appendChild(img)
+        parent.appendChild(box);
+    }
 }
 
-getGategores()
+
+
+document.querySelector(".all").onclick = function () {
+    parent.innerHTML = ""
+    all()
+}
+all()
+
+const activeClass = (lis) => {
+    lis.forEach(li => {
+    li.addEventListener("click", (e) => {
+        e.target.parentElement.querySelectorAll(".active").forEach(element => {
+            element.classList.remove("active")
+        })
+        e.target.classList.add("active")
+    })
+    });
+}

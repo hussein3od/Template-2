163114dbb8d4dbd3a26a35fc133089546da1e294
hussein3od <hhssing0@gmail.com>
2024-08-26 14:54:05 +0100

    style the page and the code

--- javaScript/info.js
+++ javaScript/info.js
@@ -9,5 +9,21 @@ console.log(param);
 const youTubeVideo = async () => {
     let data = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${param}`)
     data = await data.json();
+    console.log(data)
+    let btn = document.querySelector(".button")
+    btn.onclick = function () {
+        window.open(data.meals[0].strYoutube)
+    }
 }
 youTubeVideo()
+
+const Ingredients = async () => {
+    let data = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${param}`)
+    data = await data.json();
+    for(let i = 1; i < 5; i++){
+    let ingredient = document.createElement("h2")
+    ingredient.innerHTML = data.meals[0].strIngredient1
+    document.body.appendChild(ingredient)
+    }
+}
+Ingredients()
--- javaScript/main.js
+++ javaScript/main.js
@@ -1,5 +1,6 @@
 const parent = document.querySelector(".parent")
 let list = document.querySelector(".list")
+let country = null
 list.classList.add("class")
 const getGategores = async () => {
     document.querySelector(".loading").style.display = "block"
@@ -11,67 +12,72 @@ const getGategores = async () => {
         li.innerHTML = data.categories[i].strCategory;
         list.appendChild(li)
         li.addEventListener("click", (e) => {
-            getRespes(li.innerHTML)
+            getRespes("category", li.innerHTML)
         })
     }
     let text = document.querySelectorAll(".class li");
     activeClass(text)
 }
 
-const getRespes = async (str) => {
-    parent.innerHTML = ""
-    document.querySelector(".loading").style.display = "block"
-    let data = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${str}`)
-        data = await data.json();
-        document.querySelector(".loading").style.display = "none"
-        parent.innerHTML = ""
-        for(let m = 0; m < data.meals.length; m++){
-            let mealName = document.createElement("h2")
-            let img = document.createElement("img");
-            let box = document.createElement("div")
-            box.classList.add("box")
-            mealName.classList.add("meal-name")
-            mealName.innerHTML = data.meals[m].strMeal
-            img.src = data.meals[m].strMealThumb
-            box.appendChild(mealName)
-            box.appendChild(img)
-            parent.appendChild(box);
-        }
+const flags = async () => {
+    let data = await fetch("https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/index.json")
+    country = await data.json()
 }
-getGategores()
+const func = async () => {
+    await flags()
+    
+    getRespes("search", searchInput.value)
+}
 
-const all = async () => {
+func()
+
+let searchInput = document.querySelector(".search");
+
+const getRespes = async (type, str) => {
     parent.innerHTML = ""
-    document.querySelector(".loading").style.display = "block"
-    let data = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s")
+    document.querySelector(".loading").style.display = "block";
+    if(type === "category"){
+        data = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${str}`)
+    }else{
+        data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${str}`)
+    }
     data = await data.json();
+    parent.innerHTML = ""
     document.querySelector(".loading").style.display = "none"
     for(let m = 0; m < data.meals.length; m++){
         let mealName = document.createElement("h2")
         let img = document.createElement("img");
         let box = document.createElement("div")
         box.classList.add("box")
-        mealName.classList.add("meal-name")
-        mealName.innerHTML = data.meals[m].strMeal
-        img.src = data.meals[m].strMealThumb
-        box.appendChild(mealName)
+        mealName.classList.add("meal-name");
+        mealName.innerHTML = data.meals[m].strMeal;
+        img.src = data.meals[m].strMealThumb;
+        for(let i = 0; i < country.length; i++){
+            if(country[i].name === data.meals[m].strArea){
+                console.log("gjff");
+            }else{
+                console.log("ffef");
+                break;
+            }
+        }
         box.appendChild(img)
+        box.appendChild(mealName);
         parent.appendChild(box);
-        const url = "http://127.0.0.1:5501/HTML/info.html?";
         const obj = {
             id: data.meals[m].idMeal
         };
         const searchParams = new URLSearchParams(obj).toString();
         box.onclick = function () {
-            window.location.href = url + searchParams
+            window.location.href = window.location.origin + "/HTML/info.html?" + searchParams
         }
     }
 }
+getGategores()
+
 document.querySelector(".all").onclick = function () {
     parent.innerHTML = ""
-    all()
+    getRespes("search", searchInput.value)
 }
-all()
 
 const activeClass = (lis) => {
     lis.forEach(li => {
@@ -84,28 +90,6 @@ const activeClass = (lis) => {
     });
 }
 
-let searchInput = document.querySelector(".search");
-
-const search = async () => {
-    parent.innerHTML = "";
-    searchValue = searchInput.value
-    document.querySelector(".loading").style.display = "block"
-    let data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
-    data = await data.json();
-    document.querySelector(".loading").style.display = "none";
-    for(let m = 0; m < data.meals.length; m++){
-        let mealName = document.createElement("h2")
-        let img = document.createElement("img");
-        let box = document.createElement("div")
-        box.classList.add("box")
-        mealName.classList.add("meal-name")
-        mealName.innerHTML = data.meals[m].strMeal
-        img.src = data.meals[m].strMealThumb
-        box.appendChild(mealName)
-        box.appendChild(img)
-        parent.appendChild(box);
-    }
-}
 searchInput.onkeyup = function () {
-    search()
+    getRespes("search", searchInput.value)
 }
